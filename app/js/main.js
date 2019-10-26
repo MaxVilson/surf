84d60a6ee3039a1d5634d020c088d9c3f6b81298
Max <maxim00066@gmail.com>
2019-10-26 19:49:26 +0300

    create page two

--- app/js/main.js
+++ app/js/main.js
@@ -11,5 +11,20 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider'
+    });
+
+    $('.surf__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="surf__slider-arrow-left" src="img/arrow-left.svg" alt="arrow">',
+        nextArrow: '<img class="surf__slider-arrow-right" src="img/arrow-right.svg" alt="arrow">',
+        asNavFor: '.surf__slider-dots'
+    });
+
+    $('.surf__slider-dots').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        asNavFor: '.surf__slider',
+        focusOnSelect: true
     })
 });
